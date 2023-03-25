fd67b3c864736c386f50d66dac194cffb8fd817e
Forte Zhuo <hedikasmanto@gmail.com>
2023-03-25 07:06:29 +0700

    Fix Description

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://fortezhuo.my.id",
   integrations: [
     tailwind(),
     image({
